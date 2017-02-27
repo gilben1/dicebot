f981eb800995f228516d893b72a52310e63f5abd
Nick Gilbert <nickgilbert2@gmail.com>
2017-02-27 10:02:17 -0800

    Cleaned up cmds.bash

--- cmds.bash
+++ cmds.bash
@@ -13,8 +13,6 @@ if [[ $chan == dicebot ]] ; then
     fi
 
     if `echo $saying | grep -i '\broll\b' > /dev/null` ; then
-        #output=`echo $nick $channel $saying | ./roll_priv.bash`
-        #output=`echo $nick $chan $saying | ./roll_priv.bash`
         output=`echo $nick $chan $saying | ./roll.bash`
         echo "$output"
     elif `echo $saying | grep -i '\bhelp\b' > /dev/null` ; then
@@ -42,21 +40,16 @@ if [[ $chan == dicebot ]] ; then
 # Regular channel commands
 else
     if `echo $saying | grep -i '\bdicebot\b' > /dev/null` ; then
-        if `echo $saying | grep -i '\bhelp\b' > /dev/null` ; then
-            #echo "PRIVMSG $chan $nick: This is a bot that simulates rolling dice!"
-            #echo "PRIVMSG $chan $nick: Rolling format: 'dicebot: roll #d#+#d#+...+#d#' or '!roll #d#+#d#+...#d#'"
-            #echo "PRIVMSG $chan $nick: Each dice can be prefixed by number of dice. Can add individual numbers as well"
-            #echo "PRIVMSG $chan $nick: Valid rolls: '2d6' '2d6+10' '1d6+2d10+10+5d10' 'd20'"
-            #echo "PRIVMSG $chan $nick: use dicebot: commands to see all possible commands"
+        if `echo $saying | grep -i '\bhelp\b' > /dev/null` ; then # HELP
             echo "PRIVMSG $chan $nick: Please visit http://web.cecs.pdx.edu/~nickg/dicebothelp.txt for help using dicebot"
-        elif `echo $saying | grep -i '\broll\b' > /dev/null` ; then
+        elif `echo $saying | grep -i '\broll\b' > /dev/null` ; then # ROLL
             output=`echo $nick $chan $saying | ./roll.bash`
             echo "$output" 		
-        elif `echo $saying | grep -i '\bwho\b' > /dev/null` ; then
+        elif `echo $saying | grep -i '\bwho\b' > /dev/null` ; then # WHO
             echo "PRIVMSG $chan $nick: I am a souless automatan created by gilben. This command pings gilben, so be sure to spam it as much as possible"
-        elif `echo $saying | grep -i '\bcommands\b' > /dev/null` ; then
+        elif `echo $saying | grep -i '\bcommands\b' > /dev/null` ; then # COMMANDS
             echo "PRIVMSG $chan $nick: Dicebot Commands: commands, help, !roll, roll, stats, who"
-        elif `echo $saying | grep -i '\bstats\b' > /dev/null` ; then
+        elif `echo $saying | grep -i '\bstats\b' > /dev/null` ; then # STATS
             
             sum=$(head -n 1 ./logs/sum.log)
             sumlong=`echo $sum | sed -re ' :restart ; s/([0-9])([0-9]{3})($|[^0-9])/\1,\2\3/ ; t restart '`
@@ -68,7 +61,7 @@ else
 
 
         fi
-    elif `echo $saying | grep -i '\!roll\b' > /dev/null` ; then
+    elif `echo $saying | grep -i '\!roll\b' > /dev/null` ; then # ROLL AGAIN
         output=`echo $nick $chan $saying | ./roll.bash`
         echo "$output"
     fi
