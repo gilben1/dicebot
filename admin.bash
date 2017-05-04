1a97f0cbdfd9086f92cb619a21aee590900b45b5
Nick Gilbert <nickgilbert2@gmail.com>
2017-05-04 11:30:33 -0700

    Added say functions
    Copied the new version of admin.bash from parentheses bot, which includes
    not reading in chan since it is not needed
    
    Added say, privsay, and say-to in dicebot commands

--- admin.bash
+++ admin.bash
@@ -1,63 +1,78 @@
 #!/bin/bash
 
-read nick chan saying
+read nick saying
+
+# Send message to indicated channel
+# $1 = channel to send to
+# $2 = message to send
+function say()
+{
+    echo "PRIVMSG $1 :$2"
+}
+
+# Send private message to current user
+# $1 = message to send
+function privsay()
+{
+    echo "PRIVMSG ${nick//:} :$1"
+}
+
+# Send a '/me' ACT signal to indicated channel
+# $1 = channel to send to
+# $2 = action to do
+function act()
+{
+    echo "PRIVMSG $1 :ACTION $2"
+}
+
 
 if `echo "$saying" | grep -i '^!join\b' > /dev/null` ; then # JOIN CHANNEL
     channel=`echo "$saying" | cut -d '#' -f 2 | cut -d ' ' -f 1`
     pass=`echo "$saying" | cut -d '#' -f 2 | cut -d ' ' -f 2`
-
     if [[ $pass == $channel ]] ; then
         pass=""
     fi
     echo "JOIN #$channel $pass"
-    echo "PRIVMSG ${nick//:} :attempting to join #$channel"
+    privsay "attempting to join #$channel"
 
 elif `echo "$saying" | grep -i '^!part\b' > /dev/null` ; then # LEAVE CHANNEL
     channel=`echo "$saying" | cut -d '#' -f 2 | cut -d ' ' -f 1`
     echo "PART #$channel"
-    echo "PRIVMSG ${nick//:} :attempting to leave #$channel"
+    privsay "attempting to leave #$channel"
 
-elif `echo "$saying" | grep -i '^!reset\b' > /dev/null` ; then # RESET LOGS
-    echo 0 > ./data/sum.log
-    echo 0 > ./data/dice.log 	
-    rm ./data/users.log
 elif `echo "$saying" | grep -i '^!autojoin\b' > /dev/null` ; then # Set Autojoin
     channel=`echo "$saying" | cut -d '#' -f 2 | cut -d ' ' -f 1`
     pass=`echo "$saying" | cut -d '#' -f 2 | cut -d ' ' -f 2`
-
     if [[ $pass == $channel ]] ; then
         pass=""
     fi
 
     if ! grep -q "#$channel" ./data/autojoin.txt ; then
         echo "#$channel $pass" >> ./data/autojoin.txt
-        echo "PRIVMSG ${nick//:} :Adding #$channel to autojoin file"
+        privsay "Adding #$channel to autojoin file"
     else
-        echo "PRIVMSG ${nick//:} :#$channel is already in the autojoin file"
+        privsay "#$channel is already in the autojoin file"
     fi
 
 elif `echo "$saying" | grep -i '^!autoremove\b' > /dev/null` ; then # Remove from autojoin
     channel=`echo "$saying" | cut -d '#' -f 2 | cut -d ' ' -f 1`
     line=$(grep -n "#$channel" ./data/autojoin.txt | cut -d : -f 1)
     if [ -z $line ] ; then
-        echo "PRIVMSG ${nick//:} :#$channel was not found in the autojoin file"
+        privsay "#$channel was not found in the autojoin file"
     else
         sed -i "$line d" ./data/autojoin.txt
-        echo "PRIVMSG ${nick//:} :#$channel has been removed from the autojoin file"
+        privsay "#$channel has been removed from the autojoin file"
     fi
-
 elif `echo "$saying" | grep -i '^!autolist\b' > /dev/null` ; then # List autojoin channels
     output="Channels in autojoin.txt: "
     while read p; do
         output="$output \"$p\""
     done <./data/autojoin.txt
-    echo "PRIVMSG ${nick//:} :$output"
-
+    privsay "$output"
 elif `echo "$saying" | grep -i '^!puppet\b' > /dev/null` ; then # PUPPET
     index=0
     delim=2
     parser=`echo "$saying" | cut -d ' ' -f $delim`
-    #while `echo $parser | grep '#' > /dev/null` ; do
     while [[ ${parser:0:1} == "#" ]] ; do
         channels[$index]=$parser
         let "index+=1"
@@ -66,33 +81,22 @@ elif `echo "$saying" | grep -i '^!puppet\b' > /dev/null` ; then # PUPPET
     done
      
     act=0
-    roll=0
     let "newdim=$delim-1"
     if `echo "$saying" | grep -i '\/me' > /dev/null` ; then
         message=`echo "$saying" | cut -d '#' -f $newdim | cut -d ' ' -f 3-5000`
         act=1
-    elif `echo "$saying" | grep -i '\broll\b' > /dev/null` ; then
-        message=`echo "$saying" | cut -d '#' -f $newdim | cut -d ' ' -f 3`
-        roll=1
     else
         message=`echo "$saying" | cut -d '#' -f $newdim | cut -d ' ' -f 2-5000`
     fi
 
     for i in "${channels[@]}"
     do
-        if [[ $act == 0 ]] && [[ $roll == 0 ]] ; then
-            echo "PRIVMSG $i :$message"
-            echo "PRIVMSG ${nick//:} :Sent message to $i"
-        elif [[ $roll == 0 ]] ; then
-            echo "PRIVMSG $i :ACTION $message"
-            echo "PRIVMSG ${nick//:} :Sent message to $i"
+        if [[ $act == 0 ]] ; then
+            say "$i" "$message"
+            privsay "Sent message to $i"
         else
-            echo "PRIVMSG $i :roll $message"
-            
-            output=`echo ":dicebot" $i $message | ./roll.bash`
-            echo "$output"
-            echo "PRIVMSG ${nick//:} :Sent message to $i"
+            act "$i" "$message"
+            privsay "Sent message to $i"
         fi
     done
 fi
-
--- cmds.bash
+++ cmds.bash
@@ -4,29 +4,49 @@
 read nick chan saying
 
 . ./.config
+
+function say()
+{
+    echo "PRIVMSG $chan :$1"
+}
+
+function privsay()
+{
+    echo "PRIVMSG ${nick//:} :$1"
+}
+
+function say-to()
+{
+    echo "PRIVMSG $chan $1: $2"
+}
+
+
 botnick=$(head -n 1 ./data/botnick.txt)
 regex="\b${botnick}\b"
 
 # Private messages
 if [[ $chan == $botnick ]] ; then
     if [[ $nick == $admin ]] ; then	# Admin commands
-        output=`echo "$nick" "$chan" "$saying" | ./admin.bash`
+        output=`echo "$nick" "$saying" | ./admin.bash`
         echo "$output"
     fi
 
     if `echo $saying | grep -i '\broll\b' > /dev/null` ; then
-        output=`echo $nick $chan $saying | ./roll.bash`
+        output=`echo "$nick" "$chan" "$saying" | ./roll.bash`
         echo "$output"
     elif `echo $saying | grep -i '\bhelp\b' > /dev/null` ; then # Help docs
-        echo "NOTIFY ${nick//:} :Please visit http://web.cecs.pdx.edu/~nickg/dicebothelp.txt for help using dicebot"
+        #echo "NOTIFY ${nick//:} :Please visit http://web.cecs.pdx.edu/~nickg/dicebothelp.txt for help using dicebot"
+        privsay "Please visit http://web.cecs.pdx.edu/~nickg/dicebothelp.txt"
     elif `echo $saying | grep -i '\bstats\b' > /dev/null` ; then # display stats
         sum=$(head -n 1 ./data/sum.log)
         sumlong=`echo $sum | sed -re ' :restart ; s/([0-9])([0-9]{3})($|[^0-9])/\1,\2\3/ ; t restart '`
-        echo "PRIVMSG ${nick//:} :Total sum of all dice rolled: $(printf "%0.3E\n" $sum) or $sumlong"
+        #echo "PRIVMSG ${nick//:} :Total sum of all dice rolled: $(printf "%0.3E\n" $sum) or $sumlong"
+        privsay "Total sum of all dice rolled: $(printf "%0.3E\n" $sum) or $sumlong"
 
         tot=$(head -n 1 ./data/dice.log)
         totlong=`echo $tot | sed -re ' :restart ; s/([0-9])([0-9]{3})($|[^0-9])/\1,\2\3/ ; t restart '`
-        echo "PRIVMSG ${nick//:} :Total number of dice rolled: $(printf "%0.3E\n" $tot) or $totlong"
+        #echo "PRIVMSG ${nick//:} :Total number of dice rolled: $(printf "%0.3E\n" $tot) or $totlong"
+        privsay "Total number of dice rolled: $(printf "%0.3E\n" $tot) or $totlong"
 
         if `echo $saying | grep -i '\busers\b' > /dev/null` ; then # Get users
             out="Users of dicebot:"
@@ -35,32 +55,39 @@ if [[ $chan == $botnick ]] ; then
                 out="$out $p"	
                 let "num+=1"
             done <./data/users.log
-            echo "PRIVMSG ${nick//:} :$num $out"
+            #echo "PRIVMSG ${nick//:} :$num $out"
+            privsay "$num $out"
         fi
     fi
 # Regular channel commands
 elif ! grep -Fxqi "${nick//:}" ./data/blacklist.txt ; then
     if echo "$saying" | grep -i "$regex" > /dev/null ; then
         if echo "$saying" | grep -i '\bhelp\b' > /dev/null ; then # HELP
-            echo "PRIVMSG $chan $nick: Please visit http://web.cecs.pdx.edu/~nickg/dicebothelp.txt for help using dicebot"
+            #echo "PRIVMSG $chan $nick: Please visit http://web.cecs.pdx.edu/~nickg/dicebothelp.txt for help using dicebot"
+            say-to "$nick" "Please visit http://web.cecs.pdx.edu/~nickg/dicebothelp.txt for help using dicebot"
         elif echo "$saying" | grep -i '\broll\b' > /dev/null ; then # ROLL
-            output=`echo $nick $chan $saying | ./roll.bash`
+            output=`echo "$nick" "$chan" "$saying" | ./roll.bash`
             echo "$output" 		
         elif echo "$saying" | grep -i '\bwho\b' > /dev/null ; then # WHO
-            echo "PRIVMSG $chan $nick: I am a souless automatan created by gilben. This command pings gilben, so be sure to spam it as much as possible"
+            #echo "PRIVMSG $chan $nick: I am a souless automatan created by gilben. This command pings gilben, so be sure to spam it as much as possible"
+            say-to "$nick" "I am a souless automatan created by gilben. This command pings gilben, so be sure to spam it as much as possible"
         elif echo "$saying" | grep -i '\bcommands\b' > /dev/null ; then # COMMANDS
-            echo "PRIVMSG $chan $nick: Dicebot Commands: commands, help, !roll, roll, stats, who"
+            #echo "PRIVMSG $chan $nick: Dicebot Commands: commands, help, !roll, roll, stats, who"
+            say-to "$nick" "Dicebot Commands: commands, help, !roll, roll, stats, who"
         elif echo "$saying" | grep -i '\bstats\b' > /dev/null ; then # STATS
             
             sum=$(head -n 1 ./data/sum.log)
             sumlong=`echo $sum | sed -re ' :restart ; s/([0-9])([0-9]{3})($|[^0-9])/\1,\2\3/ ; t restart '`
-            echo "PRIVMSG $chan $nick: Total sum of all dice rolled: $(printf "%0.3E\n" $sum) or $sumlong"
+            #echo "PRIVMSG $chan $nick: Total sum of all dice rolled: $(printf "%0.3E\n" $sum) or $sumlong"
+            say-to "$nick" "Total sum of all dice rolled: $(printf "%0.3E\n" $sum) or $sumlong"
 
             tot=$(head -n 1 ./data/dice.log)
             totlong=`echo $tot | sed -re ' :restart ; s/([0-9])([0-9]{3})($|[^0-9])/\1,\2\3/ ; t restart '`
-            echo "PRIVMSG $chan $nick: Total number of dice rolled: $(printf "%0.3E\n" $tot) or $totlong"
+            #echo "PRIVMSG $chan $nick: Total number of dice rolled: $(printf "%0.3E\n" $tot) or $totlong"
+            say-to "$nick" "Total number of dice rolled: $(printf "%0.3E\n" $tot) or $totlong"
         elif echo "$saying" | grep -i '\bsource\b' > /dev/null ; then # SOURCE code on GITHUB
-            echo "PRIVMSG $chan $nick: Dicebot source code: https://gitlab.com/gilben/dicebot"
+            #echo "PRIVMSG $chan $nick: Dicebot source code: https://gitlab.com/gilben/dicebot"
+            say-to "$nick" "Dicebot source code: https://gitlab.com/gilben/dicebot"
         fi
     elif echo "$saying" | grep -i '\!roll\b' > /dev/null ; then # ROLL AGAIN
         output=`echo "$nick" "$chan" "$saying" | ./roll.bash`
