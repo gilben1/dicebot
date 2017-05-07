4b8baf144555cd345284c3b3e4070b7d813b8b14
Nick Gilbert <nickgilbert2@gmail.com>
2017-05-06 17:59:14 -0700

    Changes echo into greps into own function
    Function is called has, and is effectively the same as before
    Makes each condition much more readable

--- cmds.bash
+++ cmds.bash
@@ -27,6 +27,14 @@ function say-to()
     echo "PRIVMSG $chan $1: $2"
 }
 
+# Usage: has "$thing to search" 'regex'
+# Returns if thing 2 is found in thing 1
+function has()
+{
+    echo "$1" | grep -i $2 > /dev/null
+}
+
+
 botnick=$(head -n 1 ./data/botnick.txt)
 regex="\b${botnick}\b"
 
@@ -37,12 +45,12 @@ if [[ $chan == $botnick ]] ; then
         echo "$output"
     fi
 
-    if `echo $saying | grep -i '\broll\b' > /dev/null` ; then
+    if has "$saying" '\broll\b' ; then
         output=`echo "$nick" "$chan" "$saying" | ./roll.bash`
         echo "$output"
-    elif `echo $saying | grep -i '\bhelp\b' > /dev/null` ; then # Help docs
+    elif has "$saying" '\bhelp\b' ; then
         privsay "Please visit http://web.cecs.pdx.edu/~nickg/dicebothelp.txt"
-    elif `echo $saying | grep -i '\bstats\b' > /dev/null` ; then # display stats
+    elif has "$saying" '\bstats\b' ; then
         sum=$(head -n 1 ./data/sum.log)
         sumlong=`echo $sum | sed -re ' :restart ; s/([0-9])([0-9]{3})($|[^0-9])/\1,\2\3/ ; t restart '`
         privsay "Total sum of all dice rolled: $(printf "%0.3E\n" $sum) or $sumlong"
@@ -51,7 +59,7 @@ if [[ $chan == $botnick ]] ; then
         totlong=`echo $tot | sed -re ' :restart ; s/([0-9])([0-9]{3})($|[^0-9])/\1,\2\3/ ; t restart '`
         privsay "Total number of dice rolled: $(printf "%0.3E\n" $tot) or $totlong"
 
-        if `echo $saying | grep -i '\busers\b' > /dev/null` ; then # Get users
+        if has "$saying" '\busers\b' ; then
             out="Users of dicebot:"
             num=0
             while read p; do
@@ -63,17 +71,17 @@ if [[ $chan == $botnick ]] ; then
     fi
 # Regular channel commands
 elif ! grep -Fxqi "${nick//:}" ./data/blacklist.txt ; then
-    if echo "$saying" | grep -i "$regex" > /dev/null ; then
-        if echo "$saying" | grep -i '\bhelp\b' > /dev/null ; then # HELP
+    if has "$saying" "$regex" ; then
+        if has "$saying" '\bhelp\b' ; then
             say-to "$nick" "Please visit http://web.cecs.pdx.edu/~nickg/dicebothelp.txt for help using dicebot"
-        elif echo "$saying" | grep -i '\broll\b' > /dev/null ; then # ROLL
+        elif has "$saying" '\broll\b' ; then
             output=`echo "$nick" "$chan" "$saying" | ./roll.bash`
             echo "$output"
-        elif echo "$saying" | grep -i '\bwho\b' > /dev/null ; then # WHO
+        elif has "$saying" '\bwho\b' ; then
             say-to "$nick" "I am a souless automatan created by gilben. This command pings gilben, so be sure to spam it as much as possible"
-        elif echo "$saying" | grep -i '\bcommands\b' > /dev/null ; then # COMMANDS
+        elif has "$saying" '\bcommands\b' ; then
             say-to "$nick" "Dicebot Commands: commands, help, !roll, roll, stats, who"
-        elif echo "$saying" | grep -i '\bstats\b' > /dev/null ; then # STATS
+        elif has "$saying" '\bstats\b' ; then
             sum=$(head -n 1 ./data/sum.log)
             sumlong=`echo $sum | sed -re ' :restart ; s/([0-9])([0-9]{3})($|[^0-9])/\1,\2\3/ ; t restart '`
             say-to "$nick" "Total sum of all dice rolled: $(printf "%0.3E\n" $sum) or $sumlong"
@@ -81,10 +89,10 @@ elif ! grep -Fxqi "${nick//:}" ./data/blacklist.txt ; then
             tot=$(head -n 1 ./data/dice.log)
             totlong=`echo $tot | sed -re ' :restart ; s/([0-9])([0-9]{3})($|[^0-9])/\1,\2\3/ ; t restart '`
             say-to "$nick" "Total number of dice rolled: $(printf "%0.3E\n" $tot) or $totlong"
-        elif echo "$saying" | grep -i '\bsource\b' > /dev/null ; then # SOURCE code on GITLAB
+        elif has "$saying" '\bsource\b' ; then
             say-to "$nick" "Dicebot source code: https://gitlab.com/gilben/dicebot"
         fi
-    elif echo "$saying" | grep -i '\!roll\b' > /dev/null ; then # ROLL AGAIN
+    elif has "$saying" '\!roll\b' ; then
         output=`echo "$nick" "$chan" "$saying" | ./roll.bash`
         echo "$output"
     fi
